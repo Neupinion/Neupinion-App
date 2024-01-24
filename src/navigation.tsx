5b79fcede242fabc74b3ce0fc3f123011618fac6
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-01-24 21:30:21 +0900

    feat: MyPage 컴포넌트 추가 및 네비게이션 파라미터 리스트에 추가

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,6 +1,7 @@
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import RegisterPage from "./pages/RegisterPage";
+import MyPage from "./pages/MyPage";
 
 export type RootStackParamList = {
     Onboarding: undefined;
@@ -10,4 +11,5 @@ export type OnboardingStackParamList = {
     LoginPage: undefined;
     MainPage: undefined;
     RegisterPage: undefined;
+    MyPage: undefined;
 }
---
+++ src/pages/MyPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, View, Text} from "react-native";
+
+interface MyPageProps {}
+const MyPage:React.FC<MyPageProps> = () => {
+    return (
+        <View>
+            <Text>RegisterPage</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
+
+export default MyPage;
