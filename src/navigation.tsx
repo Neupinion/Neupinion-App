4429de46cbf27e1163389ac6084d8a77260c38b6
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-01-24 21:27:56 +0900

    feat: Onboarding되고 있는 컴포넌트를 구분하기 위한 네비게이션.tsx 작성

---
+++ src/navigation.tsx
@@ -0,0 +1,13 @@
+import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
+import RegisterPage from "./pages/RegisterPage";
+
+export type RootStackParamList = {
+    Onboarding: undefined;
+}
+
+export type OnboardingStackParamList = {
+    LoginPage: undefined;
+    MainPage: undefined;
+    RegisterPage: undefined;
+}
