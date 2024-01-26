f27721cea312964c9f572838faa48e23b9ffbcd2
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-01-26 16:34:50 +0900

    config: ESLint 상세 설정 및 필요 모듈 설치
    npm install --save-dev eslint-plugin-react @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,68 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    'env': {
+        'es6': true,
+        'node': true,
+        'jest': true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
     ],
-    "overrides": [
+    'overrides': [
         {
-            "env": {
-                "node": true
+            'env': {
+                'node': true
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
+            'files': [
+                '.eslintrc.{js,cjs}'
             ],
-            "parserOptions": {
-                "sourceType": "script"
+            'parserOptions': {
+                'sourceType': 'script'
             }
         }
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'project': './tsconfig.json',
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
     },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
+    'plugins': [
+        'react-hooks',
+        '@typescript-eslint',
+        'react'
     ],
-    "rules": {
-        "indent": [
-            "error",
-            4
+    'rules': {
+        'indent': [
+            'error',
+            2,
+            {SwitchCase:1}
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
+        'linebreak-style': [
+            'error',
+            'unix'
         ],
-        "quotes": [
-            "error",
-            "double"
+        'quotes': [
+            'error',
+            'single',
+            {avoidEscape:true}
         ],
-        "semi": [
-            "error",
-            "always"
-        ]
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-empty-function': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        'react/display-name': 'off',
+        'react/prop-types': 'off',
+    },
+    'settings': {
+        'react':{
+            'version':'detect',
+        }
     }
 };
