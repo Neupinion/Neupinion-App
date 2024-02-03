c1a3e893fe0a42e807458011db967d4d93041166
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-03 20:15:43 +0900

    feat: 더미 데이터 선언(이후 삭제) 및, 테스트 페이지에서 보여지는 방식 제작

---
+++ src/dummy/FakeNewsDummy.ts
@@ -0,0 +1,34 @@
+import { ReWriteNews } from "../shared/types/news";
+
+const fakeNewsList: ReWriteNews[] = [
+  {
+    id: 1,
+    title: '“펜타곤 대형 폭발”…美증시 출렁"',
+    tags: ['국제'],
+    date: '2023.11.03',
+    imageUrl: 'https://picsum.photos/266/107',
+  },
+  {
+    id: 2,
+    title: '“펜타곤 대형 폭발”…美증시 출렁"',
+    tags: ['국제'],
+    date: '2023.11.03',
+    imageUrl: 'https://picsum.photos/266/107',
+  },
+  {
+    id: 3,
+    title: '“펜타곤 대형 폭발”…美증시 출렁"',
+    tags: ['국제'],
+    date: '2023.11.03',
+    imageUrl: 'https://picsum.photos/266/107',
+  },
+  {
+    id: 4,
+    title: '“펜타곤 대형 폭발”…美증시 출렁"',
+    tags: ['국제'],
+    date: '2023.11.03',
+    imageUrl: 'https://picsum.photos/266/107',
+  },
+];
+
+export default fakeNewsList;
--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import theme from '../shared/styles/theme';
+import FakeIssueSlider from '../features/remakeissue/components/FakeIssueSlider';
+import fakeNewsDummy from '../dummy/FakeNewsDummy';
+import FakeIssueItem from '../features/remakeissue/components/FakeIssueItem';
 
 const TestPage = () => {
-  return <View>안녕</View>;
+  const fakeNewsList = fakeNewsDummy;
+
+  return (
+    <View style={styles.container}>
+      <FakeIssueSlider fakeNews={fakeNewsList} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.color.black,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default TestPage;
