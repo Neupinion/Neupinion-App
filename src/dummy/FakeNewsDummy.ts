88f1e17626e7aeea9603e8297e2a42d3c34369d9
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-04 03:03:13 +0900

    refactor: prettier 적용

--- src/dummy/FakeNewsDummy.ts
+++ src/dummy/FakeNewsDummy.ts
@@ -1,29 +1,29 @@
-import { ReWriteNews } from "../shared/types/news";
+import { ReWriteNews } from '../shared/types/news';
 
 const fakeNewsList: ReWriteNews[] = [
   {
-    id: "1",
+    id: '1',
     title: '“펜타곤 대형 폭발”…美증시 출렁"',
     tags: ['국제'],
     date: '2023.11.03',
     imageUrl: 'https://picsum.photos/266/107',
   },
   {
-    id: "2",
+    id: '2',
     title: '“펜타곤 대형 폭발”…美증시 출렁"',
     tags: ['국제'],
     date: '2023.11.03',
     imageUrl: 'https://picsum.photos/266/107',
   },
   {
-    id: "3",
+    id: '3',
     title: '“펜타곤 대형 폭발”…美증시 출렁"',
     tags: ['국제'],
     date: '2023.11.03',
     imageUrl: 'https://picsum.photos/266/107',
   },
   {
-    id: "4",
+    id: '4',
     title: '“펜타곤 대형 폭발”…美증시 출렁"',
     tags: ['국제'],
     date: '2023.11.03',
