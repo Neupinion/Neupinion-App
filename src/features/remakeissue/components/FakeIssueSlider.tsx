67f637f78b589c41e19639ea0414a84e77ac348a
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-03 17:24:08 +0900

    feat: EsLint 수정 및, props 전달을 통해, 슬라이더에서 어떤 컨텐츠 보여줄지 구성

--- src/features/remakeissue/components/FakeIssueSlider.tsx
+++ src/features/remakeissue/components/FakeIssueSlider.tsx
@@ -1,15 +1,15 @@
-import React, { ReactElement } from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
-import theme from "../../../shared/styles/theme";
+import React, { ReactElement } from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
 
 interface FakeIssueProps {
   children: ReactElement | ReactElement[];
 }
 
-const FakeIssueSlider = ({children}: FakeIssueProps) => {
+const FakeIssueSlider = ({ children }: FakeIssueProps) => {
   return (
     <View style={styles.container}>
-
+      <View>{children}</View>
+      <View>인디케이터</View>
     </View>
   );
 };
