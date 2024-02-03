adc720a77a906607f5c84c266b08c6477092c371
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-03 16:11:15 +0900

    feat: 공유된 theme.ts 파일 추가

---
+++ src/shared/styles/theme.ts
@@ -0,0 +1,31 @@
+interface ColorPalette {
+  main: string;
+  sub: string;
+  black: string;
+  gray: string;
+  gray1: string;
+  gray2: string;
+  gray3: string;
+  gray4: string;
+  white: string;
+}
+
+interface Theme {
+  color: ColorPalette;
+}
+
+const theme: Theme = {
+  color: {
+    main: '#7E58E9',
+    sub: '#FF5730',
+    black: '#0A0A0A',
+    gray: '#191926',
+    gray1: '#4A4A5A',
+    gray2: '#7E7D7A',
+    gray3: '#D3D3D3',
+    gray4: '#EFEFEF',
+    white: '#FFFFFF',
+  },
+};
+
+export default theme;
