510b8f2b5b3dd898f3394a924bfdf99bd7c9842f
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-04 02:52:58 +0900

    feat: 타입 변경 및 애니메이션 상수 생성. 컴포넌트 예시 제작

--- src/features/remakeissue/components/FakeIssueItem.tsx
+++ src/features/remakeissue/components/FakeIssueItem.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Animated, Image, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { ReWriteNews } from '../../../shared/types/news';
 import theme from '../../../shared/styles/theme';
+import { ITEM_SIZE, SPACER_ITEM_SIZE } from '../constants/cardAniSize';
 
 interface FakeIssueItemProps {
-  fakeNews: ReWriteNews;
+  item: ReWriteNews;
+  index: number;
+  scrollX: Animated.Value;
 }
 
-const FakeIssueItem = ({ fakeNews }: FakeIssueItemProps) => {
-  const { id, title, tags, date, imageUrl } = fakeNews;
+const FakeIssueItem = ({ item, index, scrollX }: FakeIssueItemProps) => {
+  if (!item.imageUrl) {
+    return <View style={{ width: SPACER_ITEM_SIZE }}></View>;
+  }
+
+  const inputRange = [(index - 2) * ITEM_SIZE, (index - 1) * ITEM_SIZE, index * ITEM_SIZE];
+
+  const translateY = scrollX.interpolate({
+    inputRange,
+    outputRange: [40, 0, 40],
+  });
+
+  const rotate = scrollX.interpolate({
+    inputRange,
+    outputRange: ['3deg', '0deg', '-3deg'],
+  });
+
   return (
-    <View style={styles.card}>
-      <View>{id}</View>
-      <View>{imageUrl}</View>
-      <View>{title}</View>
-      <View>{tags}</View>
-      <View>{date}</View>
-    </View>
+    <TouchableOpacity style={{ width: ITEM_SIZE }}>
+      <Animated.View
+        style={[
+          styles.animatedCard,
+          {
+            transform: [{ rotate }, { translateY }],
+          },
+        ]}
+      >
+        <TouchableOpacity onPress={() => {}}>
+          <View style={styles.card}></View>
+        </TouchableOpacity>
+      </Animated.View>
+    </TouchableOpacity>
   );
 };
 
@@ -24,9 +49,14 @@ const styles = StyleSheet.create({
   card: {
     width: 226,
     height: 189,
-    backgroundColor: theme.color.black,
+    backgroundColor: theme.color.white,
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 10,
+  },
+  animatedCard: {
+    marginHorizontal: 50,
+    alignItems: 'center',
   },
 });
 
