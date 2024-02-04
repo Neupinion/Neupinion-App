82dc626eb2ba9575c187e481e8494c12c0a30a07
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-04 21:27:46 +0900

    feat: FlatList 구현

--- src/features/remakeissue/components/CategoryCard.tsx
+++ src/features/remakeissue/components/CategoryCard.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, FlatList, } from "react-native";
 import Theme from "../../../shared/styles/theme";
 
 const CategoryCard = () => {
+  const [Items, setItems] = useState([
+    {name: '펜타곤 대형 폭발...미증시 출렁', img: 'https://reactnative.dev/img/tiny_logo.png' },
+    {name: '세계적인 기업이 환경 지속 가능성을 강조하는 새로운 이니셔티브 출범',},
+    {name: 'Item 3',},
+  ]);
+
   return (
     <View style={styles.categorycontainer}>
       <View>
         <Text style={styles.firsttext}>카테고리</Text>
       </View>
+
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.FlatListContainer}
+        data={Items}
+        renderItem={({item}) => (
+        )}
+          />
     </View>
   );
 };
