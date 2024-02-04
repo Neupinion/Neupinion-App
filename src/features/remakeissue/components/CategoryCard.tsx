07327356ab228456edd107525305a398dcece6d9
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-04 21:23:03 +0900

    style: firsttext 추가

--- src/features/remakeissue/components/CategoryCard.tsx
+++ src/features/remakeissue/components/CategoryCard.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, StyleSheet} from "react-native";
+import { View, StyleSheet, Text } from "react-native";
+import Theme from "../../../shared/styles/theme";
 
 const CategoryCard = () => {
   return (
     <View style={styles.categorycontainer}>
-
+      <View>
+        <Text style={styles.firsttext}>카테고리</Text>
+      </View>
     </View>
   );
 };
@@ -14,6 +17,11 @@ const styles = StyleSheet.create({
     //backgroundColor: 'tomato',
     marginLeft: 25,
     marginTop: 40,
+  },
+  firsttext:{
+    fontSize: 16,
+    fontWeight: '700',
+    color: Theme.color.white,
   },
 });
 export default CategoryCard;
