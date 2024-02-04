d2ddb76a63864024c467d8bf64708235ccac6f38
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-04 21:20:25 +0900

    style: categorycontainer 추가

--- src/features/remakeissue/components/CategoryCard.tsx
+++ src/features/remakeissue/components/CategoryCard.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import { Text, View, StyleSheet, FlatList, ImageBackground } from "react-native";
-import { useState } from 'react';
-import Theme from "../../../shared/styles/theme";
+import { View, StyleSheet} from "react-native";
+
 const CategoryCard = () => {
-
-  const [Items, setItems] = useState([
-    {name: '펜타곤 대형 폭발...미증시 출렁', img: 'https://reactnative.dev/img/tiny_logo.png' },
-    {name: '세계적인 기업이 환경 지속 가능성을 강조하는 새로운 이니셔티브 출범',},
-    {name: 'Item 3',},
-  ]);
-
   return (
     <View style={styles.categorycontainer}>
 
