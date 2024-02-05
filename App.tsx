d43c8a9e585d90926435d1318af3e37f3ba6aaa3
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-05 22:43:13 +0900

    config: clear git cache

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CategoryCard from "./src/features/remakeissue/components/CategoryCard";
 
 export default function App(): JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+        <CategoryCard></CategoryCard>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,7 @@ export default function App(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
