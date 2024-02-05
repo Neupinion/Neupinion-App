2296ecc1fe749c58b4530e7d059537e9bd81c8b2
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-06 01:35:09 +0900

    Revert "config: clear git cache"
    This reverts commit d43c8a9e585d90926435d1318af3e37f3ba6aaa3.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import CategoryCard from "./src/features/remakeissue/components/CategoryCard";
 
 export default function App(): JSX.Element {
   return (
     <View style={styles.container}>
-      <View>
-        <CategoryCard></CategoryCard>
-      </View>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,7 +14,7 @@ export default function App(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
