f2773f4d45c29697211dd4b35eb07843454db4f5
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-11 00:41:28 +0900

    config: axio 설치

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App(): JSX.Element {
+  useEffect(() => {
+    const requestGet = async () => {
+      try {
+        const response = await axios.get('https://47e4ff49-58f3-4ed5-b1dd-4089300a86aa.mock.pstmn.io/reprocessed-issue');
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    requestGet();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
