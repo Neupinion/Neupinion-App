58e3b3590f4c45090a70219e46bc0e2914c1a7b3
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-13 13:36:18 +0900

    feat: Modal 에서 선택되는 기능 제작

--- src/dateSelect/components/DateModal.tsx
+++ src/dateSelect/components/DateModal.tsx
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 import { Calendar, DateData, LocaleConfig } from 'react-native-calendars';
 import { koreaLocales } from '../constants/locales';
-import { cvtParamDate } from "../constants/cvtParamDate";
+import { cvtParamDate } from '../constants/cvtParamDate';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 LocaleConfig.locales['kr'] = koreaLocales;
@@ -21,8 +21,19 @@ interface DateModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
+
 const DateModal: React.FC<DateModalProps> = ({ isOpen, onClose }) => {
   const [modalY] = useState(new Animated.Value(Dimensions.get('window').height));
+  const [selectedDate, setSelectedDate] = useState(cvtParamDate(new Date()));
+
+  const onDaySelect = (day: DateData) => {
+    const date = day.dateString;
+    setSelectedDate(date);
+  };
+
+  const markedDates = {
+    [selectedDate]: { selected: true, selectedColor: '#7E58E9' },
+  };
 
   useEffect(() => {
     if (isOpen) {
@@ -61,11 +72,12 @@ const DateModal: React.FC<DateModalProps> = ({ isOpen, onClose }) => {
                 style={styles.calendar}
                 firstDay={1}
                 monthFormat={'yyyy년 MM월'}
-                onDayLongPress={(day) => {}}
-                onMonthChange={(month) => {}}
+                onDayPress={onDaySelect}
+                markedDates={markedDates}
                 hideExtraDays={true}
                 disableMonthChange={true}
                 maxDate={cvtParamDate(new Date())}
+                disableAllTouchEventsForDisabledDays={true}
                 theme={{
                   selectedDayBackgroundColor: '#7E58E9',
                   arrowColor: 'rgba(255, 255, 255, 0.8)',
@@ -93,7 +105,7 @@ const DateModal: React.FC<DateModalProps> = ({ isOpen, onClose }) => {
 const styles = StyleSheet.create({
   container: {
     width: Dimensions.get('window').width,
-    height: 520,
+    height: 480,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
     backgroundColor: '#21202F',
