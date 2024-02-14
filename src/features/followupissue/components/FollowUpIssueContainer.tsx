b4f6f921402f7e1b66d6e21a21f1da2c979111d3
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-14 20:55:29 +0900

    feat: 부주제 선택 UI 제작 및 theme 수정

--- src/features/followupissue/components/FollowUpIssueContainer.tsx
+++ src/features/followupissue/components/FollowUpIssueContainer.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { getMainCategoryName, MainCategory } from '../functions/getMainCategoryName';
 import { getSubCategoryName, SubCategory } from '../functions/getSubCategoryName';
-import { Dimensions, StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Animated,
+  ScrollView,
+} from 'react-native';
 import React from 'react';
+import theme from '../../../shared/styles/theme';
 
 const FollowUpIssueContainer = () => {
   const [topTab, setTopTab] = useState<MainCategory>(MainCategory.All);
   const [subTab, setSubTab] = useState<SubCategory>(SubCategory.Entertainment);
-
   const changeTopTab = (newTab: MainCategory) => {
     console.log('메인 카테고리 변경:', MainCategory[newTab]);
     setTopTab(newTab);
@@ -25,8 +33,8 @@ const FollowUpIssueContainer = () => {
         const category = MainCategory[key as keyof typeof MainCategory];
         return (
           <TouchableOpacity key={category} onPress={() => changeTopTab(category)}>
-            <View style={styles.button}>
-              <Text style={styles.buttonText}>{getMainCategoryName(Number(key))}</Text>
+            <View style={styles.mainCategoryButton}>
+              <Text style={styles.mainCategoryText}>{getMainCategoryName(Number(key))}</Text>
             </View>
           </TouchableOpacity>
         );
@@ -38,10 +46,13 @@ const FollowUpIssueContainer = () => {
       .filter((key) => !isNaN(Number(key)))
       .map((key) => {
         const category = SubCategory[key as keyof typeof SubCategory];
+        const isSelected = category === subTab;
+        const buttonStyle = isSelected ? styles.subCategoryButtonOn : styles.subCategoryButtonOff;
+        const textStyle = isSelected ? styles.subCategoryTextOn : styles.subCategoryTextOff;
         return (
           <TouchableOpacity key={category} onPress={() => changeSubTab(category)}>
-            <View style={styles.button}>
-              <Text style={styles.buttonText}>{getSubCategoryName(Number(key))}</Text>
+            <View style={buttonStyle}>
+              <Text style={textStyle}>{getSubCategoryName(Number(key))}</Text>
             </View>
           </TouchableOpacity>
         );
@@ -51,7 +62,10 @@ const FollowUpIssueContainer = () => {
   return (
     <View style={styles.container}>
       <View style={styles.mainCategoryContainer}>{renderMainCategoryButtons()}</View>
-      <View style={styles.subCategoryContainer}>{renderSubCategoryButtons()}</View>
+      <View style={styles.mainUnderLine} />
+      <ScrollView horizontal={true}>
+        <View style={styles.subCategoryContainer}>{renderSubCategoryButtons()}</View>
+      </ScrollView>
     </View>
   );
 };
@@ -59,27 +73,77 @@ const FollowUpIssueContainer = () => {
 const styles = StyleSheet.create({
   container: {
     width: Dimensions.get('window').width,
+    marginHorizontal: 10,
     marginTop: 16,
-    paddingHorizontal: 16,
-    paddingBottom: 16,
   },
   mainCategoryContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'flex-start',
     marginBottom: 16,
   },
+  mainCategoryButton: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginLeft: 18,
+    gap: 12,
+  },
+  mainCategoryText: {
+    color: 'white',
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: 24,
+    letterSpacing: -0.48,
+  },
+  mainUnderLine: {
+    width: Dimensions.get('window').width,
+    height: 1,
+    backgroundColor: 'rgba(226, 226, 226, 0.1)',
+  },
   subCategoryContainer: {
+    display: 'flex',
+    marginTop: 21,
+    marginLeft: 10,
     flexDirection: 'row',
     justifyContent: 'space-around',
     flexWrap: 'wrap',
   },
-  button: {
-    padding: 10,
-    borderRadius: 4,
-    marginHorizontal: 4,
+  subCategoryButtonOn: {
+    display: 'flex',
+    marginRight: 10,
+    height: 30,
+    paddingHorizontal: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    backgroundColor: theme.color.gray4,
+    borderWidth: 0.8,
+    borderColor: 'rgba(189, 189, 189, 0)',
   },
-  buttonText: {
-    color: 'white',
+  subCategoryButtonOff: {
+    display: 'flex',
+    marginRight: 10,
+    height: 30,
+    paddingHorizontal: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+    borderRadius: 30,
+    borderWidth: 0.8,
+    borderColor: 'rgba(189, 189, 189, 0.5)',
+  },
+  subCategoryTextOn: {
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    color: '#BDBDBD',
+  },
+  subCategoryTextOff: {
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    color: 'rgba(189, 189, 189, 0.5)',
   },
 });
 
