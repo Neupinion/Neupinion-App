7e846f5b67b4301c9c7b3907f8ccb08a25a0d9d9
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-14 15:48:21 +0900

    feat: MainPage 내에서 필요한 요소들 정의

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,19 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Dimensions,
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import theme from '../shared/styles/theme';
 import FakeIssueSlider from '../features/remakeissue/components/FakeIssueSlider';
 import GlobalTextStyles from '../shared/styles/GlobalTextStyles';
 import ReProcessedIssueDummy from '../dummy/ReProcessedIssueDummy';
 import FollowUpIssueDummy from '../dummy/FollowUpIssueDummy';
+import { WithLocalSvg } from 'react-native-svg';
+import MainArrowSvg from '../assets/icon/mainarrow.svg';
 
 const MainPage = () => {
   const reprocessedIssue = ReProcessedIssueDummy;
   const followUpIssue = FollowUpIssueDummy;
 
+  const pressArrow = () => {
+    console.log('화살표 누름');
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={GlobalTextStyles.NormalText17}>가짜뉴스 이슈(타이틀 변경 예정)</Text>
+      <View style={styles.titleContainer}>
+        <Text style={GlobalTextStyles.NormalText17}>새로운 후속보도가 있어요!</Text>
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={GlobalTextStyles.NormalText17}>가짜뉴스 이슈(타이틀 변경 예정)</Text>
+      </View>
       <FakeIssueSlider fakeNews={reprocessedIssue} />
+      <View style={styles.titleContainer}>
+        <Text style={GlobalTextStyles.NormalText17}>카테고리1</Text>
+        <TouchableOpacity onPress={pressArrow}>
+          <WithLocalSvg
+            style={styles.svgStyle}
+            width={14}
+            height={14}
+            asset={MainArrowSvg as ImageSourcePropType}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={GlobalTextStyles.NormalText17}>카테고리2</Text>
+        <TouchableOpacity onPress={pressArrow}>
+          <WithLocalSvg
+            style={styles.svgStyle}
+            width={14}
+            height={14}
+            asset={MainArrowSvg as ImageSourcePropType}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.divideLine}></View>
+      <View style={styles.titleContainer}>
+        <Text style={GlobalTextStyles.NormalText17}>후속이슈</Text>
+        <TouchableOpacity onPress={pressArrow}>
+          <WithLocalSvg
+            style={styles.svgStyle}
+            width={14}
+            height={14}
+            asset={MainArrowSvg as ImageSourcePropType}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -24,6 +76,25 @@ const styles = StyleSheet.create({
     backgroundColor: theme.color.black,
     alignItems: 'flex-start',
     justifyContent: 'center',
+  },
+  titleContainer: {
+    width: Dimensions.get('window').width,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+  svgStyle: {
+    width: 24,
+    height: 24,
+    marginRight: 21,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  divideLine: {
+    width: Dimensions.get('window').width,
+    height: 10,
+    marginVertical: 12,
+    flexShrink: 0,
+    backgroundColor: '#21202F',
   },
 });
 
