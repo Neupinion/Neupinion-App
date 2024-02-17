7da7c3642dbea734bcf61f38973be7256409cc69
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-17 22:54:09 +0900

    feat: NormalText16 추가

--- src/shared/styles/GlobalTextStyles.ts
+++ src/shared/styles/GlobalTextStyles.ts
@@ -3,6 +3,7 @@ import { TextStyle } from 'react-native';
 
 interface GlobalTextStyles {
   NormalText17: TextStyle;
+  NormalText16: TextStyle;
 }
 
 const GlobalTextStyles: GlobalTextStyles = {
@@ -16,7 +17,15 @@ const GlobalTextStyles: GlobalTextStyles = {
     letterSpacing: -0.51,
     marginLeft: 26,
   },
+  NormalText16: {
+    flex: 1,
+    fontSize: 16,
+    color: theme.color.white,
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: 24,
+    letterSpacing: -0.48,
+  },
 };
-
 
 export default GlobalTextStyles;
