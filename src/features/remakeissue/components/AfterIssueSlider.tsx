7c643cab181f7fd74d50f827f2a8caae35ee5ddb
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-17 21:44:29 +0900

    feat: AfterIssueSlider.tsx 추가

---
+++ src/features/remakeissue/components/AfterIssueSlider.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const AfterIssueSlider = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to React Native with TypeScript!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default AfterIssueSlider;
