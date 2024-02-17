f98d94302b2f80fa606b42a0b0cfccfd6b2de2df
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-17 23:31:06 +0900

    feat: NormalText14,16 삭제

--- src/shared/styles/GlobalTextStyles.ts
+++ src/shared/styles/GlobalTextStyles.ts
@@ -3,8 +3,6 @@ import { TextStyle } from 'react-native';
 
 interface GlobalTextStyles {
   NormalText17: TextStyle;
-  NormalText16: TextStyle;
-  NormalText14: TextStyle;
 }
 
 const GlobalTextStyles: GlobalTextStyles = {
@@ -17,24 +15,6 @@ const GlobalTextStyles: GlobalTextStyles = {
     lineHeight: 25.5,
     letterSpacing: -0.51,
     marginLeft: 26,
-  },
-  NormalText16: {
-    flex: 1,
-    fontSize: 16,
-    color: theme.color.white,
-    fontStyle: 'normal',
-    fontWeight: '700',
-    lineHeight: 24,
-    letterSpacing: -0.48,
-  },
-  NormalText14: {
-    flex: 1,
-    fontSize: 14,
-    color: theme.color.white,
-    fontStyle: 'normal',
-    fontWeight: '500',
-    lineHeight: 21,
-    letterSpacing: -0.42,
   },
 };
 
