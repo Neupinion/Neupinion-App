5b29a312c033f0fe3468f193830aedf97f46a109
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-17 23:14:53 +0900

    feat: NormalText14,16 적용

--- src/features/remakeissue/components/AfterIssueItem.tsx
+++ src/features/remakeissue/components/AfterIssueItem.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import GlobalTextStyles from "../../../shared/styles/GlobalTextStyles";
 
 const AfterIssueItem = () => {
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={() => {}}>
-        <Text>Press me</Text>
+        <Text style={GlobalTextStyles.NormalText16}>전소연, 마약 루머 부인 "사실무근, 강력 법적 대응"</Text>
+        <Text style={GlobalTextStyles.NormalText14}>"전소연 마약 연루설"에 대한 정정보도가 나왔어요</Text>
       </TouchableOpacity>
     </View>
   );
@@ -13,9 +15,14 @@ const AfterIssueItem = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: 338,
+    height: 80,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingHorizontal: 20,
+    gap: 7,
+    borderRadius: 10,
+    backgroundColor: '#7E58E9',
   },
 });
 
