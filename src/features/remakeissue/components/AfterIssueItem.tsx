ec9b605ff4b4ff549c9fdbf14c42aa96d6397e8a
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-18 00:00:18 +0900

    feat: props 전달 받게 수정

--- src/features/remakeissue/components/AfterIssueItem.tsx
+++ src/features/remakeissue/components/AfterIssueItem.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Animated } from "react-native";
 import GlobalTextStyles from "../../../shared/styles/GlobalTextStyles";
+import theme from "../../../shared/styles/theme";
+import { FollowUpIssue, ReProcessedIssue } from "../../../shared/types/news";
 
-const AfterIssueItem = () => {
+interface AfterIssueItemProps {
+  item: FollowUpIssue;
+}
+
+const AfterIssueItem = ({item}:AfterIssueItemProps) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={() => {}}>
-        <Text style={GlobalTextStyles.NormalText16}>전소연, 마약 루머 부인 "사실무근, 강력 법적 대응"</Text>
-        <Text style={GlobalTextStyles.NormalText14}>"전소연 마약 연루설"에 대한 정정보도가 나왔어요</Text>
+        <Text style={styles.titleText} numberOfLines={1} ellipsizeMode="tail">{item.title}</Text>
+        <View style={{ marginBottom: 7 }} />
+        <Text style={styles.titleUnderText}>"전소연 마약 연루설"에 대한 정정보도가 나왔어요"</Text>
       </TouchableOpacity>
     </View>
   );
@@ -19,11 +26,29 @@ const styles = StyleSheet.create({
     height: 80,
     justifyContent: 'center',
     alignItems: 'center',
+    alignContent: 'center',
     paddingHorizontal: 20,
     gap: 7,
     borderRadius: 10,
     backgroundColor: '#7E58E9',
   },
+  titleText: {
+    fontSize: 16,
+    color: theme.color.white,
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: 24,
+    letterSpacing: -0.48,
+  },
+  titleUnderText: {
+    fontSize: 14,
+    color: theme.color.white,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    lineHeight: 21,
+    letterSpacing: -0.42,
+  },
+
 });
 
 export default AfterIssueItem;
