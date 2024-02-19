ea18e1a44946b70df7b10ae232bd0aeb4baed3d0
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-19 22:11:07 +0900

    feat: AfterIssueSItem Props 이용하기

--- src/features/remakeissue/components/AfterIssueItem.tsx
+++ src/features/remakeissue/components/AfterIssueItem.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Animated } from "react-native";
-import GlobalTextStyles from "../../../shared/styles/GlobalTextStyles";
-import theme from "../../../shared/styles/theme";
-import { FollowUpIssue, ReProcessedIssue } from "../../../shared/types/news";
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from "react-native";
+import theme from '../../../shared/styles/theme';
+import { FollowUpIssue } from '../../../shared/types/news';
 
 interface AfterIssueItemProps {
   item: FollowUpIssue;
 }
 
-const AfterIssueItem = ({item}:AfterIssueItemProps) => {
+const AfterIssueItem = ({ item }: AfterIssueItemProps) => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => {}}>
-        <Text style={styles.titleText} numberOfLines={1} ellipsizeMode="tail">{item.title}</Text>
+      <TouchableOpacity style={styles.card} onPress={() => {}}>
+        <Text style={styles.titleText} numberOfLines={1} ellipsizeMode="tail">
+          {item.title}
+        </Text>
         <View style={{ marginBottom: 7 }} />
-        <Text style={styles.titleUnderText}>"전소연 마약 연루설"에 대한 정정보도가 나왔어요"</Text>
+        <Text style={styles.titleUnderText}>
+          "{item.reprocessedIssueTitle}"에 대한 정정보도가 나왔어요"
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -22,15 +25,11 @@ const AfterIssueItem = ({item}:AfterIssueItemProps) => {
 
 const styles = StyleSheet.create({
   container: {
-    width: 338,
+    width: Dimensions.get("window").width,
     height: 80,
     justifyContent: 'center',
     alignItems: 'center',
     alignContent: 'center',
-    paddingHorizontal: 20,
-    gap: 7,
-    borderRadius: 10,
-    backgroundColor: '#7E58E9',
   },
   titleText: {
     fontSize: 16,
@@ -48,7 +47,17 @@ const styles = StyleSheet.create({
     lineHeight: 21,
     letterSpacing: -0.42,
   },
-
+  card: {
+    width: 338,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    paddingHorizontal: 20,
+    gap: 7,
+    borderRadius: 10,
+    backgroundColor: '#7E58E9',
+  },
 });
 
 export default AfterIssueItem;
