212364ce3be32ec9892a6aabcf30b7c554fd6427
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-20 14:40:26 +0900

    feat: DateProvider 제공

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import MainPage from './src/pages/MainPage';
+import { DateProvider } from "./src/features/date/DateProvider";
 
 export default function App(): JSX.Element {
-  return <MainPage />;
+  return (
+    <DateProvider>
+      <MainPage />
+    </DateProvider>
+  );
 }
