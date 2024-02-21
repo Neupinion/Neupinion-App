40b2616e104312e55ba988ec6787fa983d8ed95e
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-21 23:39:28 +0900

    feat: 후속 이슈 개수에 따라 다른 UI 구현

--- src/features/remakeissue/components/AfterIssueSlider.tsx
+++ src/features/remakeissue/components/AfterIssueSlider.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, FlatList, Dimensions } from 'react-native';
+import React, { useMemo, useState } from "react";
+import { View, StyleSheet, FlatList, Dimensions, Text, ImageSourcePropType } from 'react-native';
 import { FollowUpIssue } from '../../../shared/types/news';
 import AfterIssueItem from './AfterIssueItem';
 import Indicator from './Indicator';
+import theme from '../../../shared/styles/theme';
+import RightArrowSvg from '../../../assets/icon/rightarrow.svg';
+import { WithLocalSvg } from 'react-native-svg';
+import { invisibleLeftCardData, invisibleRightCardData } from "../constants/invisibleCardData";
 
 interface AfterIssueProps {
   afterNews: FollowUpIssue[] | null;
@@ -10,25 +14,47 @@ interface AfterIssueProps {
 const AfterIssueSlider = ({ afterNews }: AfterIssueProps) => {
   const [slideIndex, setSlideIndex] = useState(0);
 
+  const preparedafterNews = useMemo(() => {
+    if (!afterNews) return;
+    const newItem = {
+      id: 9,
+      title: '내가 투표한 기사의 새소식을 알려드려요!',
+      reprocessedIssueTitle: '내가 투표한 기사의 새소식을 알려드려요!',
+    };
+
+    return [...afterNews, newItem];
+  }, [afterNews]);
+
   return (
     <View style={styles.container}>
-      <FlatList
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        snapToInterval={Dimensions.get('window').width}
-        data={afterNews}
-        keyExtractor={(item) => String(item.id)}
-        renderItem={({ item }) => <AfterIssueItem item={item} />}
-        onScroll={(event) => {
-          const newIndex = Math.round(
-            event.nativeEvent.contentOffset.x / Dimensions.get('window').width,
-          );
-          setSlideIndex(newIndex);
-        }}
-        decelerationRate="fast"
-        bounces={false}
-      />
-      <Indicator data={afterNews} slideIndex={slideIndex} />
+      {!afterNews || afterNews.length === 0 ? (
+        <View style={styles.boxContainer}>
+          <View style={styles.card}>
+            <Text style={styles.titleText}> 더 많은 후속보도 확인하기 </Text>
+            <View style={styles.circle}>
+              <WithLocalSvg width={15} height={13} asset={RightArrowSvg as ImageSourcePropType} />
+            </View>
+          </View>
+        </View>
+      ) : (
+        <FlatList
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={Dimensions.get('window').width}
+          data={preparedafterNews}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <AfterIssueItem item={item} />}
+          onScroll={(event) => {
+            const newIndex = Math.round(
+              event.nativeEvent.contentOffset.x / Dimensions.get('window').width,
+            );
+            setSlideIndex(newIndex);
+          }}
+          decelerationRate="fast"
+          bounces={false}
+        />
+      )}
+      {/*<Indicator data={afterNews} slideIndex={slideIndex} />*/}
     </View>
   );
 };
@@ -38,7 +64,41 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    //marginLeft: 26,
+  },
+  boxContainer: {
+    width: Dimensions.get('window').width,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  card: {
+    width: 338,
+    height: 80,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    borderRadius: 10,
+    backgroundColor: theme.color.main,
+  },
+  titleText: {
+    fontSize: 16,
+    color: theme.color.white,
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: 24,
+    letterSpacing: -0.48,
+  },
+  circle: {
+    width: 33,
+    height: 33,
+    borderRadius: 50,
+    backgroundColor: theme.color.main,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: theme.color.white,
   },
 });
 
