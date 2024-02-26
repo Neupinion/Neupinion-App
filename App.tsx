41cc06fef4104f079a8253f497b09a268d523e7f
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-26 20:53:26 +0900

    feat: App.tsx 파일 내에서, 폰트를 로딩한다.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainPage from './src/pages/MainPage';
 import { DateProvider } from './src/features/date/provider/DateProvider';
+import * as Font from 'expo-font';
+import { FontSource } from 'expo-font';
+
+interface FontMap {
+  [name: string]: FontSource;
+}
 
 export default function App(): JSX.Element {
-  return (
-    <DateProvider>
-      <MainPage />
-    </DateProvider>
-  );
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function loadFontAsync() {
+      const fontMap: FontMap = {
+        pretendardBlack: require('./assets/fonts/Pretendard-Black.ttf'),
+      };
+      try {
+        await Font.loadAsync(fontMap);
+        setIsReady(true);
+      } catch (error) {
+        console.error('Font loading error:', error);
+      }
+    }
+
+    loadFontAsync();
+  }, []);
+
+  return <DateProvider>{isReady && <MainPage />}</DateProvider>;
 }
