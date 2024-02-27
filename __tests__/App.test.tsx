4a82330877f3cdbcf18c10ef9e922182df497193
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-02-27 11:25:51 +0900

    config: 테스트 환경설정

--- __tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import App from '../App';
-
-it('has 1 child', () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
