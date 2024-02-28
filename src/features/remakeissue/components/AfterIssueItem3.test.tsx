0f3f81490bf4664be1a556d94af458b223b2e72e
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-02-28 10:09:33 +0900

    feat: 후속 이슈를 최신순으로 정렬 하는지 확인하는 테스트 코드

--- src/features/remakeissue/components/AfterIssueItem3.test.tsx
+++ src/features/remakeissue/components/AfterIssueItem3.test.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react-native';
-import AfterIssueItem from './AfterIssueItem';
-import { FlatList } from 'react-native';
+import AfterIssueSlider from './AfterIssueSlider';
 
-describe('AfterIssueItem component', () => {
+describe('AfterIssueSlider component', () => {
   it('renders items in chronological order based on createdAt time', async () => {
     // Define sample data with createdAt time
-    const data = [
+    const afterNews = [
       {
         id: '1',
         title: 'First Item',
@@ -30,27 +29,17 @@ describe('AfterIssueItem component', () => {
       },
     ];
 
-    // Render FlatList with AfterIssueItem component for each data item
-    const { queryAllByTestId } = render(
-      // Change this line
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <AfterIssueItem item={item} />}
-      />,
-    );
+    // Render AfterIssueSlider with sample data
+    render(<AfterIssueSlider afterNews={afterNews} />);
 
     // Wait for the component to finish rendering
     await waitFor(() => {});
 
-    // Get the list of rendered items
-    const renderedItems = queryAllByTestId('afterIssueItem');
-
     // Check if items are rendered in chronological order based on createdAt time
-    for (let i = 0; i < data.length - 1; i++) {
-      const currentItemTime = new Date(data[i].createdAt).getTime();
-      const nextItemTime = new Date(data[i + 1].createdAt).getTime();
-      expect(currentItemTime).toBeGreaterThanOrEqual(nextItemTime);
+    for (let i = 0; i < afterNews.length - 1; i++) {
+      const currentItemTime = new Date(afterNews[i].createdAt).getTime();
+      const nextItemTime = new Date(afterNews[i + 1].createdAt).getTime();
+      expect(currentItemTime).toBeLessThanOrEqual(nextItemTime);
     }
   });
 });
