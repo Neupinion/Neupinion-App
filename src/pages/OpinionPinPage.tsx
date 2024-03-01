bb793736da7199c3644a9d31c83404390746dbd4
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-02 00:38:25 +0900

    feat: 핀을 선택하는 페이지 제작

---
+++ src/pages/OpinionPinPage.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import {
+  Dimensions,
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import theme from '../shared/styles/theme';
+import { WithLocalSvg } from 'react-native-svg';
+import OpinionBackButton from '../assets/icon/opinionbackbutton.svg';
+import OpinionCheckButton from '../assets/icon/opinionpurplecheck.svg';
+
+const OpinionPinPage = () => {
+  const onClickBackButton = () => {
+    console.log('뒤로가기');
+  };
+
+  const onClickCheckButton = () => {
+    console.log('내용이 없다면 알럿을...');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <TouchableOpacity style={styles.topSvgStyle} onPress={onClickBackButton}>
+          <WithLocalSvg width={10} height={20} asset={OpinionBackButton as ImageSourcePropType} />
+        </TouchableOpacity>
+        <Text style={styles.topTextStyle}></Text>
+        <TouchableOpacity style={styles.topSvgStyle} onPress={onClickCheckButton}>
+          <WithLocalSvg width={17} height={12} asset={OpinionCheckButton as ImageSourcePropType} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.color.black,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  topContainer: {
+    width: Dimensions.get('window').width,
+    height: 30,
+    marginTop: 66,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topSvgStyle: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  topTextStyle: {
+    width: 234,
+    color: theme.color.white,
+    marginHorizontal: 30,
+    textAlign: 'center',
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: 24,
+    letterSpacing: -0.48,
+  },
+});
+
+export default OpinionPinPage;
