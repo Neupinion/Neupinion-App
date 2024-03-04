a996c50081a3056dd322f2f978a7b672a93ed0aa
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-04 15:00:57 +0900

    feat: 뉴스 보기 버튼 추가

--- src/pages/OpinionPostPage.tsx
+++ src/pages/OpinionPostPage.tsx
@@ -15,11 +15,12 @@ import PinButton from '../features/opinionpost/components/PinButton';
 import PinTextNumberContainer from '../features/opinionpost/components/PinTextNumberContainer';
 import OpinionWriteContainer from '../features/opinionpost/components/OpinionWriteContainer';
 import SentenceBox from '../features/opinionpost/components/SentenceBox';
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { RootStackParamList } from '../rootStackParamList';
-
+import { StackNavigationProp } from '@react-navigation/stack';
 
 const OpinionPostPage = () => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
   type ScreenRouteProp = RouteProp<RootStackParamList, 'OpinionPost'>;
   const route = useRoute<ScreenRouteProp>();
 
@@ -30,6 +31,10 @@ const OpinionPostPage = () => {
 
   const onClickCheckButton = () => {
     console.log('내용이 없다면 알럿을...');
+  };
+
+  const onClickShowNewsButton = () => {
+    navigation.navigate('OpinionPin');
   };
 
   return (
@@ -44,17 +49,28 @@ const OpinionPostPage = () => {
         </TouchableOpacity>
       </View>
       <View style={styles.choosePinContainer}>
-        <PinTextNumberContainer
-          circleNumber={1}
-          circleText={'의견을 남길 부분을 선택해주세요'}
-          isActivate={true}
-        />
+        <View style={styles.pinFirstTextContainer}>
+          <PinTextNumberContainer
+            circleNumber={1}
+            circleText={'의견을 남길 부분을 선택해주세요'}
+            isActivate={true}
+          />
+          {sentenceIndex !== undefined && (
+            <TouchableOpacity style={styles.showNewsButton} onPress={onClickShowNewsButton}>
+              <Text style={styles.showNewsText}>뉴스보기</Text>
+            </TouchableOpacity>
+          )}
+        </View>
         {sentenceIndex === undefined && <PinButton />}
         {sentenceIndex !== undefined && <SentenceBox sentenceNumber={sentenceIndex} />}
       </View>
       <View style={styles.choosePinContainer}>
-        <PinTextNumberContainer circleNumber={2} circleText={'생각 쓰기'} isActivate={false} />
-        <OpinionWriteContainer isActivate={false} />
+        <PinTextNumberContainer
+          circleNumber={2}
+          circleText={'생각 쓰기'}
+          isActivate={sentenceIndex !== undefined}
+        />
+        <OpinionWriteContainer isActivate={sentenceIndex !== undefined} />
       </View>
     </View>
   );
@@ -100,6 +116,27 @@ const styles = StyleSheet.create({
     marginTop: 20,
     gap: 16,
     width: 338,
+  },
+  pinFirstTextContainer: {
+    flexDirection: 'row',
+    gap: 14,
+  },
+  showNewsButton: {
+    display: 'flex',
+    paddingVertical: 2,
+    paddingHorizontal: 8,
+    justifyContent: 'center',
+    alignItems: 10,
+    borderRadius: 5,
+    backgroundColor: 'rgba(126, 88, 233, 0.2)',
+  },
+  showNewsText: {
+    color: theme.color.main,
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    lineHeight: 21,
+    letterSpacing: -0.42,
   },
 });
 
