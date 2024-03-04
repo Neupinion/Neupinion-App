068b81998c12e079d1491ec46a5e1453803f72cc
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-04 16:38:05 +0900

    feat: Navigation.tsx 생성 후, 코드분할

--- App.tsx
+++ App.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
-import MainPage from './src/pages/MainPage';
-import { DateProvider } from './src/features/date/provider/DateProvider';
-import OpinionPostPage from './src/pages/OpinionPostPage';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import OpinionPinPage from './src/pages/OpinionPinPage';
+import Navigation from './src/Navigation';
 
-const Stack = createStackNavigator();
 export default function App(): JSX.Element {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="OpinionPost" component={OpinionPostPage} />
-        <Stack.Screen name="OpinionPin" component={OpinionPinPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
---
+++ src/Navigation.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OpinionPostPage from './pages/OpinionPostPage';
+import OpinionPinPage from './pages/OpinionPinPage';
+import MainPage from "./pages/MainPage";
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Group>
+          <Stack.Screen name="OpinionPost" component={OpinionPostPage}></Stack.Screen>
+          <Stack.Screen name="OpinionPin" component={OpinionPinPage}></Stack.Screen>
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default Navigation;
