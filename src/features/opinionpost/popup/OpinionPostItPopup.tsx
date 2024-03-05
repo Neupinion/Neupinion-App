8b4706fb8fc657c1c52345677c48367500f3995a
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-06 00:22:59 +0900

    feat: 핀 svg 추가

--- src/features/opinionpost/popup/OpinionPostItPopup.tsx
+++ src/features/opinionpost/popup/OpinionPostItPopup.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { ImageSourcePropType, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { WithLocalSvg } from 'react-native-svg';
 import DateModalClose from '../../../assets/icon/datemodalclose.svg';
-import theme from "../../../shared/styles/theme";
-import fontFamily from "../../../shared/styles/fontFamily";
+import theme from '../../../shared/styles/theme';
+import fontFamily from '../../../shared/styles/fontFamily';
+import Pin from '../../../assets/icon/pin.svg';
 
 interface OpinionPostItPopupProps {
   closeModal: () => void;
@@ -15,6 +16,10 @@ const OpinionPostItPopup: React.FC<OpinionPostItPopupProps> = ({ closeModal, tit
     closeModal();
   };
 
+  const onClickDelete = () => {
+    
+  }
+
   return (
     <Modal transparent animationType="fade">
       <View style={styles.overlay}>
@@ -24,7 +29,12 @@ const OpinionPostItPopup: React.FC<OpinionPostItPopupProps> = ({ closeModal, tit
           </TouchableOpacity>
         </View>
         <View style={styles.container}>
+          <View style={styles.pin}>
+            <WithLocalSvg width={20} height={20} asset={Pin as ImageSourcePropType} />
+          </View>
           <Text style={styles.titleText}>{title}</Text>
+          <View style={styles.dotLine} />
+          <Text style={styles.opinionText}>{opinion}</Text>
         </View>
       </View>
     </Modal>
@@ -65,12 +75,43 @@ const styles = StyleSheet.create({
   },
   titleText: {
     color: theme.color.white,
+    marginTop: 24,
+    display: 'flex',
+    width: 280,
     textAlign: 'justify',
-    fontFamily: fontFamily.pretendard.medium,
+    fontFamily: fontFamily.pretendard.bold,
+    fontSize: 15,
     fontStyle: 'normal',
     fontWeight: '600',
     lineHeight: 22.5,
     letterSpacing: -0.45,
+  },
+  dotLine: {
+    width: 280,
+    marginVertical: 16,
+    flexShrink: 0,
+    height: 0,
+    backgroundColor: '#D1D3D8',
+    borderWidth: 0.6,
+    borderStyle: 'dashed',
+  },
+  opinionText: {
+    display: 'flex',
+    color: theme.color.white,
+    width: 280,
+    fontSize: 14,
+    flexShrink: 0,
+    textAlign: 'justify',
+    fontFamily: fontFamily.pretendard.medium,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    lineHeight: 21,
+    letterSpacing: -0.42,
+  },
+  pin: {
+    position: 'absolute',
+    left: 14,
+    top: 26,
   },
 });
 
