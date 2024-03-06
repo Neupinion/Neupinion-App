e2a6bb2f9c1cfbaf8c90d71098afb00f4fd974d6
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-06 13:35:00 +0900

    feat: 카카오톡의 앱링크를 공유할 수 있는 컴포넌트 제작

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import MainPage from './src/pages/MainPage';
 import { DateProvider } from './src/features/date/provider/DateProvider';
 import { useCachedResources } from './src/useCachedResources';
+import AppLinkTestPage from "./src/pages/AppLinkTestPage";
 
 export default function App(): JSX.Element | null {
   const isLoaded = useCachedResources();
@@ -9,7 +10,7 @@ export default function App(): JSX.Element | null {
   if (isLoaded) {
     return (
       <DateProvider>
-        <MainPage />
+        <AppLinkTestPage />
       </DateProvider>
     );
   } else {
---
+++ src/pages/AppLinkTestPage.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Platform, Share, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import theme from '../shared/styles/theme';
+
+const AppLinkTestPage = () => {
+  const link =
+    Platform.OS === 'ios'
+      ? 'https://apps.apple.com/us/app/kakaotalk/id362057947'
+      : 'https://play.google.com/store/search?q=%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%86%A1%ED%86%A1&c=apps&hl=ko';
+
+  const appLinkShare = async () => {
+    try {
+      const result = await Share.share({
+        message: link,
+      });
+
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log('activityType!');
+        } else {
+          console.log('Share!');
+        }
+      } else if (result.action === Share.dismissedAction) {
+        console.log('dismissed');
+      }
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={{ width: 220, height: 110, backgroundColor: 'white' }}
+        onPress={() => appLinkShare()}
+      >
+        <Text style={{ backgroundColor: 'black' }}>공유하기</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.color.black,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default AppLinkTestPage;
