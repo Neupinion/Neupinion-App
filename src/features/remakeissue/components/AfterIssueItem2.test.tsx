86c9601528b9708ecd831baf03ff62545e21cc87
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-03-13 12:47:42 +0900

    feat: 테스트 수정

--- src/features/remakeissue/components/AfterIssueItem2.test.tsx
+++ src/features/remakeissue/components/AfterIssueItem2.test.tsx
@@ -3,22 +3,22 @@ import { render } from '@testing-library/react-native';
 import AfterIssueSlider from './AfterIssueSlider';
 
 describe('AfterIssueSlider component', () => {
-  it('renders only one item with correct text when afterNews is null or empty array', () => {
-    // Render component with empty afterNews
-    const { queryByText } = render(<AfterIssueSlider afterNews={null} />);
-
-    // Expect FlatList to render only one item
-    expect(queryByText('내가 투표한 기사의 새소식을 알려드려요!')).toBeDefined();
-    expect(queryByText('이슈에 투표를 하면, 관련 기사를 빠르게 전해드려요')).toBeDefined();
-    expect(queryByText('더 많은 후속보도 확인하기')).toBeNull();
-
-    // Render component with afterNews array of length 0
-    const { queryAllByText } = render(<AfterIssueSlider afterNews={[]} />);
-
-    // Expect FlatList to render only one item
-    expect(queryAllByText('내가 투표한 기사의 새소식을 알려드려요!').length).toBe(1);
-    expect(queryAllByText('이슈에 투표를 하면, 관련 기사를 빠르게 전해드려요').length).toBe(1);
-    expect(queryByText('더 많은 후속보도 확인하기')).toBeNull();
+  it('afterNews 배열이 empty 자료형일 때 item 이 하나만 랜더링 되는가', () => {
+    // null 일 때
+    // react-native 컴포넌트를 @testing-library/react-native 패키지의 render 함수를 통해 렌더링함
+    const screen = render(<AfterIssueSlider afterNews={null} />);
+    const text1 = screen.getByText('내가 투표한 기사의 새소식을 알려드려요!');
+    const text2 = screen.getByText('이슈에 투표를 하면, 관련 기사를 빠르게 전해드려요');
+    expect(text1).toBeDefined();
+    expect(text2).toBeDefined();
+  });
+  it('afterNews 배열이 empty 자료형일 때 item 이 하나만 랜더링 되는가', () => {
+    // empty 자료형일 떄
+    const screen = render(<AfterIssueSlider afterNews={[]} />);
+    const text1 = screen.getByText('내가 투표한 기사의 새소식을 알려드려요!');
+    const text2 = screen.getByText('이슈에 투표를 하면, 관련 기사를 빠르게 전해드려요');
+    expect(text1).toBeDefined();
+    expect(text2).toBeDefined();
   });
 
   it('renders "더 많은 후속보도 확인하기" as the titleText of the last item when afterNews is not null and not an empty array', () => {
@@ -39,12 +39,8 @@ describe('AfterIssueSlider component', () => {
         reprocessedIssueTitle: '',
       },
     ];
-    const { queryAllByText } = render(<AfterIssueSlider afterNews={afterNews} />);
-
-    // Find all titleText elements in the FlatList
-    const titleTextElements = queryAllByText('더 많은 후속보도 확인하기');
-
-    // Expect the length of titleTextElements to be 1
-    expect(titleTextElements.length).toBe(1);
+    const screen = render(<AfterIssueSlider afterNews={afterNews} />);
+    const titleText = screen.getByText('더 많은 후속보도 확인하기');
+    expect(titleText).toBeDefined();
   });
 });
