90a14441faa7159075ca0d22e9417f6bed874179
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-13 14:22:42 +0900

    chore: branch 변경을 통한 rebase 직접 작업

---
+++ src/features/opinionpost/components/PinButton.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import theme from '../../../shared/styles/theme';
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParamList } from '../../../rootStackParamList';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+const PinButton = () => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+
+  const onClickButton = () => {
+    navigation.navigate('OpinionPin');
+  };
+
+  return (
+    <TouchableOpacity onPress={onClickButton} style={styles.pinButton}>
+      <Text style={styles.pinButtonText}>핑찍기</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  pinButton: {
+    display: 'flex',
+    width: 338,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: '#394358',
+  },
+  pinButtonText: {
+    color: theme.color.white,
+    fontSize: 17,
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: 25.5,
+    letterSpacing: -0.51,
+  },
+});
+
+export default PinButton;
