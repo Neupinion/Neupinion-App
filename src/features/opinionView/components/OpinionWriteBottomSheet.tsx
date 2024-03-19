9cfeed4ad01bba928b7c1585e9364f2b919ffa3e
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-19 13:27:26 +0900

    feat: modalVisible, title, content를 props로 받아, 올라오고 내려오는 기능이 달린 바텀시트 개발

---
+++ src/features/opinionView/components/OpinionWriteBottomSheet.tsx
@@ -0,0 +1,106 @@
+import React, { useRef, useState } from 'react';
+import {
+  Animated,
+  Dimensions,
+  Modal,
+  PanResponder,
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+interface OpinionWriteBottomSheetProps {
+  modalVisible: boolean;
+  title: string;
+  content: string;
+}
+
+const OpinionWriteBottomSheet = ({
+  modalVisible,
+  title,
+  content,
+}: OpinionWriteBottomSheetProps) => {
+  const [isModalVisible, setModalVisible] = useState(modalVisible);
+  const animationDuration: number = 300;
+  const screenHeight = Dimensions.get('screen').height;
+  const panY = useRef(new Animated.Value(screenHeight)).current;
+  const translateY = panY.interpolate({
+    inputRange: [-1, 0, 1],
+    outputRange: [0, 0, 1],
+  });
+
+  const resetBottomSheet = Animated.timing(panY, {
+    toValue: 0,
+    duration: animationDuration,
+    useNativeDriver: true,
+  });
+
+  const closeBottomSheet = Animated.timing(panY, {
+    toValue: screenHeight,
+    duration: animationDuration,
+    useNativeDriver: true,
+  });
+
+  const closeModal = () => {
+    closeBottomSheet.start(() => {
+      setModalVisible(false);
+    });
+  };
+
+  const panResponders = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => false,
+      onPanResponderMove: (event, gestureState) => {
+        panY.setValue(gestureState.dy);
+      },
+      onPanResponderRelease: (event, gestureState) => {
+        if (gestureState.dy > 0 && gestureState.vy > 1.5) {
+          closeModal();
+        } else {
+          resetBottomSheet.start();
+        }
+      },
+    }),
+  ).current;
+
+  return (
+    <Modal visible={isModalVisible} animationType={'fade'} transparent statusBarTranslucent>
+      <View style={styles.overlay}>
+        <TouchableWithoutFeedback onPress={closeModal}>
+          <View style={styles.background} />
+        </TouchableWithoutFeedback>
+        <Animated.View
+          style={{ ...styles.bottomSheetContainer, transform: [{ translateY: translateY }] }}
+          {...panResponders.panHandlers}
+        >
+          <Text>{title}</Text>
+          <Text>{content}</Text>
+        </Animated.View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  },
+  bottomSheetContainer: {
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+});
+
+export default OpinionWriteBottomSheet;
