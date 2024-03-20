108820a7a17fae4fa9ee378d2cdd761b099d1397
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-20 20:50:44 +0900

    feat: 바텀시트 스타일 확정

--- src/features/opinionView/components/OpinionWriteBottomSheet.tsx
+++ src/features/opinionView/components/OpinionWriteBottomSheet.tsx
@@ -8,7 +8,13 @@ import {
   Text,
   StyleSheet,
   TouchableWithoutFeedback,
+  TouchableOpacity,
+  ImageSourcePropType,
 } from 'react-native';
+import theme from '../../../shared/styles/theme';
+import fontFamily from '../../../shared/styles/fontFamily';
+import { WithLocalSvg } from 'react-native-svg';
+import OpinionPin from '../../../assets/icon/opinionpin.svg';
 
 interface OpinionWriteBottomSheetProps {
   modalVisible: boolean;
@@ -53,6 +59,14 @@ const OpinionWriteBottomSheet = ({
     });
   };
 
+  const onClickModifyButton = () => {
+    console.log('수정버튼 클릭');
+  };
+
+  const onClickDeleteButton = () => {
+    console.log('삭제버튼 클릭');
+  };
+
   const panResponders = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
@@ -80,10 +94,24 @@ const OpinionWriteBottomSheet = ({
           style={{ ...styles.bottomSheetContainer, transform: [{ translateY: translateY }] }}
         >
           <View style={styles.panResponderContainer} {...panResponders.panHandlers}>
-            <View style={styles.panResponder}/>
+            <View style={styles.panResponder} />
           </View>
-          <Text>{title}</Text>
-          <Text>{content}</Text>
+          <View style={styles.titleContainer}>
+            <View style={styles.pinContainer}>
+              <TouchableOpacity style={styles.pin}>
+                <WithLocalSvg width={20} height={20} asset={OpinionPin as ImageSourcePropType} />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.titleText}>{title}</Text>
+          </View>
+          <View style={styles.dotLine} />
+          <Text style={styles.contentText}>{content}</Text>
+          <TouchableOpacity style={styles.modifyButton} onPress={onClickModifyButton}>
+            <Text style={styles.modifyButtonText}>수정하기</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.deleteButton} onPress={onClickDeleteButton}>
+            <Text style={styles.deleteButtonText}>삭제하기</Text>
+          </TouchableOpacity>
         </Animated.View>
       </View>
     </Modal>
@@ -118,6 +146,95 @@ const styles = StyleSheet.create({
     height: 4,
     borderRadius: 4,
     backgroundColor: 'white',
+  },
+  titleContainer: {
+    display: 'flex',
+    marginTop: 24,
+    marginHorizontal: 25,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  titleText: {
+    color: theme.color.white,
+    textAlign: 'justify',
+    fontFamily: fontFamily.pretendard.bold,
+    fontStyle: 'normal',
+    fontSize: 15,
+    fontWeight: '600',
+    lineHeight: 22.5,
+    letterSpacing: -0.45,
+  },
+  pinContainer: {
+    display: 'flex',
+    width: 20,
+    flexDirection: 'column',
+    position: 'absolute',
+    left: -5,
+    top: 2,
+  },
+  pin: {
+    width: 20,
+    height: 20,
+  },
+  dotLine: {
+    width: 340,
+    marginVertical: 16,
+    flexShrink: 0,
+    height: 0,
+    backgroundColor: '#D1D3D8',
+    borderWidth: 0.6,
+    borderStyle: 'dashed',
+  },
+  contentText: {
+    color: '#EBECF1',
+    height: 190,
+    textAlign: 'justify',
+    fontFamily: fontFamily.pretendard.bold,
+    fontStyle: 'normal',
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 21,
+    letterSpacing: -0.42,
+    marginHorizontal: 25,
+  },
+  modifyButton: {
+    width: Dimensions.get('window').width - 50,
+    backgroundColor: theme.color.main,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 28,
+    height: 50,
+  },
+  modifyButtonText: {
+    color: theme.color.white,
+    textAlign: 'justify',
+    fontFamily: fontFamily.pretendard.bold,
+    fontStyle: 'normal',
+    fontSize: 17,
+    fontWeight: '700',
+    lineHeight: 25.5,
+    letterSpacing: -0.51,
+  },
+  deleteButton: {
+    width: Dimensions.get('window').width - 50,
+    backgroundColor: '#212A3C',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 10,
+    marginBottom: 20,
+    height: 50,
+  },
+  deleteButtonText: {
+    color: '#71788F',
+    textAlign: 'justify',
+    fontFamily: fontFamily.pretendard.bold,
+    fontStyle: 'normal',
+    fontSize: 17,
+    fontWeight: '700',
+    lineHeight: 25.5,
+    letterSpacing: -0.51,
   },
 });
 
