4b6d98ad07e384e6b5c6fdb1bd2902932c593244
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-03-24 19:05:10 +0900

    feat: entext 추가

--- src/features/remakeissue/constants/reliabilty.ts
+++ src/features/remakeissue/constants/reliabilty.ts
@@ -1,6 +1,6 @@
 export const reliabilityText = [
-  { id: 1, text: '완전 신뢰' },
-  { id: 2, text: '조금 신뢰' },
-  { id: 3, text: '조금 의심' },
-  { id: 4, text: '완전 의심' },
+  { id: 1, text: '완전 신뢰', entext: 'HIGHLY_TRUSTED' },
+  { id: 2, text: '조금 신뢰',entext: 'SOMEWHAT_TRUSTED'  },
+  { id: 3, text: '조금 의심',entext: 'SOMEWHAT_DISTRUSTED'  },
+  { id: 4, text: '완전 의심',entext: 'HIGHLY_DISTRUSTED'  },
 ];
