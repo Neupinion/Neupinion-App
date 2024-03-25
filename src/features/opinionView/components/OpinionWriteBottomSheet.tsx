1b3e381d63678d4823da13ab74a27a7cc497e1ee
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-25 20:51:25 +0900

    Feat/#60 의견 포스트잇 클릭시 바텀시트 등장 및 삭제 팝업 개발 (#64)
    * config: React Native Bottom Sheet npm 의존성 설치
    
    * feat: Stack.Navigation에 DetailPage 추가
    
    * feat: modalVisible, title, content를 props로 받아, 올라오고 내려오는 기능이 달린 바텀시트 개발
    
    * feat: props를 true로 지정하면, 바텀시트가 올라오도록 개발
    
    useEffect 내용 참고
    
    * style: 임시 적용한 purple 색 해제
    
    * feat: DetailPage에 적용하는 방식
    
    * feat: panResponder 컴포넌트 추가
    
    * feat: 바텀시트 스타일 확정
    
    * feat: 경고 팝업 컴포넌트 제작 및 바텀시트 삭제하기에 적용

---
+++ src/features/opinionView/components/OpinionWriteBottomSheet.tsx
@@ -0,0 +1,252 @@
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  Animated,
+  Dimensions,
+  Modal,
+  PanResponder,
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  ImageSourcePropType,
+} from 'react-native';
+import theme from '../../../shared/styles/theme';
+import fontFamily from '../../../shared/styles/fontFamily';
+import { WithLocalSvg } from 'react-native-svg';
+import OpinionPin from '../../../assets/icon/opinionpin.svg';
+import WarningPopup from '../../popup/components/WarningPopup';
+
+interface OpinionWriteBottomSheetProps {
+  modalVisible: boolean;
+  title: string;
+  content: string;
+}
+
+const OpinionWriteBottomSheet = ({
+  modalVisible,
+  title,
+  content,
+}: OpinionWriteBottomSheetProps) => {
+  const [isModalVisible, setModalVisible] = useState(modalVisible);
+  const [isDeletePopupVisible, setIsDeletePopupVisible] = useState(false);
+
+  const screenHeight = Dimensions.get('screen').height;
+
+  const animationDuration: number = 300;
+  const panY = useRef(new Animated.Value(screenHeight)).current;
+
+  const translateY = panY.interpolate({
+    inputRange: [-1, 0, 1],
+    outputRange: [0, 0, 1],
+  });
+
+  const resetBottomSheet = Animated.timing(panY, {
+    toValue: 0,
+    duration: animationDuration,
+    useNativeDriver: true,
+  });
+
+  const closeBottomSheet = Animated.timing(panY, {
+    toValue: screenHeight,
+    duration: animationDuration,
+    useNativeDriver: true,
+  });
+
+  useEffect(() => {
+    if (isModalVisible) {
+      resetBottomSheet.start();
+    }
+  }, [isModalVisible]);
+  const closeModal = () => {
+    closeBottomSheet.start(() => {
+      setModalVisible(false);
+    });
+  };
+
+  const onClickModifyButton = () => {
+    console.log('수정버튼 클릭');
+  };
+
+  const onClickDeleteButton = () => {
+    setIsDeletePopupVisible(true);
+  };
+
+  const panResponders = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => false,
+      onPanResponderMove: (event, gestureState) => {
+        panY.setValue(gestureState.dy);
+      },
+      onPanResponderRelease: (event, gestureState) => {
+        if (gestureState.dy > 0 && gestureState.vy > 1.5) {
+          closeModal();
+        } else {
+          resetBottomSheet.start();
+        }
+      },
+    }),
+  ).current;
+
+  return (
+    <Modal visible={isModalVisible} animationType={'fade'} transparent statusBarTranslucent>
+      <View style={styles.overlay}>
+        <TouchableWithoutFeedback onPress={closeModal}>
+          <View style={styles.background} />
+        </TouchableWithoutFeedback>
+        <Animated.View
+          style={{ ...styles.bottomSheetContainer, transform: [{ translateY: translateY }] }}
+        >
+          <View style={styles.panResponderContainer} {...panResponders.panHandlers}>
+            <View style={styles.panResponder} />
+          </View>
+          <View style={styles.titleContainer}>
+            <View style={styles.pinContainer}>
+              <TouchableOpacity style={styles.pin}>
+                <WithLocalSvg width={20} height={20} asset={OpinionPin as ImageSourcePropType} />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.titleText}>{title}</Text>
+          </View>
+          <View style={styles.dotLine} />
+          <Text style={styles.contentText}>{content}</Text>
+          <TouchableOpacity style={styles.modifyButton} onPress={onClickModifyButton}>
+            <Text style={styles.modifyButtonText}>수정하기</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.deleteButton} onPress={onClickDeleteButton}>
+            <Text style={styles.deleteButtonText}>삭제하기</Text>
+          </TouchableOpacity>
+        </Animated.View>
+        <WarningPopup
+          modalVisible={isDeletePopupVisible}
+          title={'작성한 의견을 삭제하시겠습니까?'}
+          onClose={() => setIsDeletePopupVisible(false)}
+          onConfirm={() => {}}
+        />
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  },
+  bottomSheetContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#212A3C',
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+  },
+  panResponderContainer: {
+    height: 24,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  panResponder: {
+    width: 44,
+    height: 4,
+    borderRadius: 4,
+    backgroundColor: 'white',
+  },
+  titleContainer: {
+    display: 'flex',
+    marginTop: 24,
+    marginHorizontal: 25,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  titleText: {
+    color: theme.color.white,
+    textAlign: 'justify',
+    fontFamily: fontFamily.pretendard.bold,
+    fontStyle: 'normal',
+    fontSize: 15,
+    fontWeight: '600',
+    lineHeight: 22.5,
+    letterSpacing: -0.45,
+  },
+  pinContainer: {
+    display: 'flex',
+    width: 20,
+    flexDirection: 'column',
+    position: 'absolute',
+    left: -5,
+    top: 2,
+  },
+  pin: {
+    width: 20,
+    height: 20,
+  },
+  dotLine: {
+    width: 340,
+    marginVertical: 16,
+    flexShrink: 0,
+    height: 0,
+    backgroundColor: '#D1D3D8',
+    borderWidth: 0.6,
+    borderStyle: 'dashed',
+  },
+  contentText: {
+    color: '#EBECF1',
+    height: 190,
+    textAlign: 'justify',
+    fontFamily: fontFamily.pretendard.bold,
+    fontStyle: 'normal',
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 21,
+    letterSpacing: -0.42,
+    marginHorizontal: 25,
+  },
+  modifyButton: {
+    width: Dimensions.get('window').width - 50,
+    backgroundColor: theme.color.main,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 28,
+    height: 50,
+  },
+  modifyButtonText: {
+    color: theme.color.white,
+    textAlign: 'justify',
+    fontFamily: fontFamily.pretendard.bold,
+    fontStyle: 'normal',
+    fontSize: 17,
+    fontWeight: '700',
+    lineHeight: 25.5,
+    letterSpacing: -0.51,
+  },
+  deleteButton: {
+    width: Dimensions.get('window').width - 50,
+    backgroundColor: '#212A3C',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 10,
+    marginBottom: 20,
+    height: 50,
+  },
+  deleteButtonText: {
+    color: '#71788F',
+    textAlign: 'justify',
+    fontFamily: fontFamily.pretendard.bold,
+    fontStyle: 'normal',
+    fontSize: 17,
+    fontWeight: '700',
+    lineHeight: 25.5,
+    letterSpacing: -0.51,
+  },
+});
+
+export default OpinionWriteBottomSheet;
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -19,6 +19,7 @@ import OpinionWriteSlider from '../features/remakeissue/components/OpinionWriteS
 import ReliabilityEvaluation from '../features/remakeissue/components/ReliabilityEvaluation';
 import CategoryLatestNews from '../features/remakeissue/components/CategoryLatestNews';
 import ReProcessedIssueDummy from '../dummy/ReProcessedIssueDummy';
+import OpinionWriteBottomSheet from "../features/opinionView/components/OpinionWriteBottomSheet";
 
 const DetailPage = () => {
   const [bookMarkClicked, setBookMarkClicked] = useState(false);
@@ -65,6 +66,15 @@ const DetailPage = () => {
         <View style={styles.divideLine}></View>
         <CategoryLatestNews fakeNews={reprocessedIssue} />
       </ScrollView>
+      <OpinionWriteBottomSheet
+        modalVisible={true}
+        title={
+          '    블룸버그통신 등에 따르면 22일(현지 시간) 오전 9시를 전후 로 미 워싱턴DC에 있는 펜타곤으로 보이는 건물에서 검은 연기가 피어오르는 사진이 트위터를 통해 국내외로 빠르게 확산했다.'
+        }
+        content={
+          '최초로 게시된 곳이 공신력 있는 매체가 아니고 트위터라서 신뢰도가 떨어지는 듯. 계정도 그냥 개인 계정인 것 같아서 추가적인 확인이 필요할 것 같다.'
+        }
+      />
     </View>
   );
 };
