eee76da7549b7ef1c87085265952d310b78572cb
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-29 17:32:57 +0900

    refactor: Modal이 다양한 컴포넌트가 쌓여가는 구조를 가지면서, 애니메이션을 받을 수 있는 것을 고려하여 타입 재지정

--- src/shared/types/modal.ts
+++ src/shared/types/modal.ts
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Animated } from 'react-native';
+
+export interface ModalContent {
+  component: React.ReactNode;
+  openAnimation?: Animated.CompositeAnimation;
+  closeAnimation?: Animated.CompositeAnimation;
+}
 
 export interface ModalState {
   isOpen: boolean;
-  content: React.ReactNode | null;
+  content: ModalContent[];
 }
