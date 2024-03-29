bb01989fc355a4a0c3fe73051b31e49bb6c57340
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-03-29 17:34:17 +0900

    refactor: 애니메이션 및 다양한 컴포넌트 전달 가능하도록 변경

--- src/shared/hooks/useModal.ts
+++ src/shared/hooks/useModal.ts
@@ -1,36 +1,37 @@
 import React, { useCallback } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { modalState } from '../../recoil/modalState';
+import { ModalContent } from '../types/modal';
 import { Animated } from 'react-native';
 
 export const useModal = () => {
-  const setModalState = useSetRecoilState(modalState);
+  const [{ content }, setModalState] = useRecoilState(modalState);
 
   const openModal = useCallback(
-    (content: React.ReactNode, openAnimation?: Animated.CompositeAnimation) => {
-      if (openAnimation) {
-        openAnimation.start(() => {
-          setModalState({ isOpen: true, content });
-        });
-      } else {
-        setModalState({ isOpen: true, content });
-      }
+    (
+      component: React.ReactNode,
+      openAnimation?: Animated.CompositeAnimation,
+      closeAnimation?: Animated.CompositeAnimation,
+    ) => {
+      const newContent: ModalContent = { component, openAnimation, closeAnimation };
+      setModalState((prevState) => ({
+        isOpen: true,
+        content: [...prevState.content, newContent],
+      }));
+      openAnimation?.start();
     },
     [setModalState],
   );
 
-  const closeModal = useCallback(
-    (closeAnimation?: Animated.CompositeAnimation) => {
-      if (closeAnimation) {
-        closeAnimation.start(() => {
-          setModalState({ isOpen: false, content: null });
-        });
-      } else {
-        setModalState({ isOpen: false, content: null });
-      }
-    },
-    [setModalState],
-  );
+  const closeModal = useCallback(() => {
+    const lastModal = content[content.length - 1];
+    lastModal?.closeAnimation?.start(() => {
+      setModalState((prevState) => ({
+        isOpen: prevState.content.length > 1,
+        content: prevState.content.slice(0, -1),
+      }));
+    });
+  }, [content, setModalState]);
 
   return { openModal, closeModal };
 };
