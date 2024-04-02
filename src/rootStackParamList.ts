e9618778f799383188bbbffd035b4e26a72cb355
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-04-02 22:09:42 +0900

    fix: ESLint 수정

--- src/rootStackParamList.ts
+++ src/rootStackParamList.ts
@@ -1,7 +1,7 @@
 import { OpinionWrite } from './shared/types/news';
 
 export type RootStackParamList = {
-  OpinionPost: { opinionWrite?: OpinionWrite; sentenceNumber?: number; issueId: number};
+  OpinionPost: { opinionWrite?: OpinionWrite; sentenceNumber?: number; issueId: number };
   OpinionPin: undefined;
   Main: undefined;
   Detail: undefined;
