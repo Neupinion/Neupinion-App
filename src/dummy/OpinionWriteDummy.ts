447ac2d6371351d2f33e3ceed299668d52c9f256
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-04-02 20:58:56 +0900

    feat: 임시 테스트를 위한 Dummy파일 생성

---
+++ src/dummy/OpinionWriteDummy.ts
@@ -0,0 +1,9 @@
+import { OpinionWrite } from '../shared/types/news';
+
+export const OpinionWriteDummy: OpinionWrite = {
+  id: 1,
+  paragraphId: 1,
+  paragraphContent: '문단 내용',
+  content: '의견 내용',
+  isReliable: true,
+};
