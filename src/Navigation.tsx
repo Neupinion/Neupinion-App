4bb6ee31fd8a2df26d154ed3017d08fddb13eab5
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-04-07 17:35:31 +0900

    fix: Navigation.tsx 수정

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -4,33 +4,34 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OpinionPostPage from './pages/OpinionPostPage';
 import OpinionPinPage from './pages/OpinionPinPage';
 import MainPage from './pages/MainPage';
+import ModalContainer from './shared/components/ModalContainer';
 import ReprocessedIssueDetailPage from './pages/ReprocessedIssueDetailPage';
+
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Group>
-          <Stack.Screen name="MainPage" component={MainPage}></Stack.Screen>
-        </Stack.Group>
-        <Stack.Group>
-          <Stack.Screen
-            name="ReprocessedIssueDetailPage"
-            component={ReprocessedIssueDetailPage}
-          ></Stack.Screen>
-        </Stack.Group>
-
-        <Stack.Group>
-          <Stack.Screen name="OpinionPost" component={OpinionPostPage}></Stack.Screen>
-          <Stack.Screen name="OpinionPin" component={OpinionPinPage}></Stack.Screen>
-        </Stack.Group>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <ModalContainer />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Group>
+            <Stack.Screen name="MainPage" component={MainPage} />
+          </Stack.Group>
+          <Stack.Group>
+            <Stack.Screen name="DetailPage" component={ReprocessedIssueDetailPage} />
+          </Stack.Group>
+          <Stack.Group>
+            <Stack.Screen name="OpinionPost" component={OpinionPostPage} />
+            <Stack.Screen name="OpinionPin" component={OpinionPinPage} />
+          </Stack.Group>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
 export default Navigation;
