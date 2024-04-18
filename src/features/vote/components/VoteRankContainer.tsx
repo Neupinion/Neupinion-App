ac178d748eba5664252c82ef77bc2465ac8bfb33
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-04-18 14:23:22 +0900

    chore: 투표 순위 UI를 보여주는 컨테이너 컴포넌트 추가

---
+++ src/features/vote/components/VoteRankContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const VoteRankContainer = () => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+});
+
+export default VoteRankContainer;
