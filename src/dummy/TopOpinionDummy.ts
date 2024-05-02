22e7ae756f6b9ad10b0d4f694b096a61e4ed47d8
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-05-02 20:39:01 +0900

    fix: dummy 데이터 id 고유값으로 변경

--- src/dummy/TopOpinionDummy.ts
+++ src/dummy/TopOpinionDummy.ts
@@ -15,7 +15,7 @@ export const TopOpinionDummy: Opinion[] = [
     isLiked: true,
   },
   {
-    id: 1,
+    id: 2,
     memberId: 2,
     nickname: '김철수',
     profileImageUrl: 'https://www.neupinion.com/profile/1',
@@ -28,7 +28,7 @@ export const TopOpinionDummy: Opinion[] = [
     isLiked: true,
   },
   {
-    id: 1,
+    id: 3,
     memberId: 3,
     nickname: '김철수',
     profileImageUrl: 'https://www.neupinion.com/profile/1',
@@ -41,7 +41,7 @@ export const TopOpinionDummy: Opinion[] = [
     isLiked: true,
   },
   {
-    id: 1,
+    id: 4,
     memberId: 4,
     nickname: '김철수',
     profileImageUrl: 'https://www.neupinion.com/profile/1',
@@ -54,7 +54,7 @@ export const TopOpinionDummy: Opinion[] = [
     isLiked: true,
   },
   {
-    id: 1,
+    id: 5,
     memberId: 5,
     nickname: '김철수',
     profileImageUrl: 'https://www.neupinion.com/profile/1',
