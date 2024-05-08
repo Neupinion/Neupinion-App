16635cd3f3a6d32b24a44c62f5872bd735dac5fd
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-05-08 14:49:59 +0900

    fix: 가짜뉴스 이슈 카드 슬라이더 스타일 수정

--- src/features/remakeissue/components/FakeIssueIcon.tsx
+++ src/features/remakeissue/components/FakeIssueIcon.tsx
@@ -6,6 +6,7 @@ import { WithLocalSvg } from 'react-native-svg';
 import MessageIcon from '../../../assets/icon/message.svg';
 import UserIcon from '../../../assets/icon/user.svg';
 import { WINDOW_WIDTH } from '../../../shared/constants/display';
+import fontFamily from "../../../shared/styles/fontFamily";
 interface FakeIssueIconProps {
   data: ReProcessedIssue[] | null;
   slideIndex: number;
@@ -40,9 +41,10 @@ const styles = StyleSheet.create({
   },
   iconText: {
     marginLeft: 6,
+    fontFamily: fontFamily.pretendard.medium,
     fontStyle: 'normal',
     fontSize: 14,
-    color: theme.color.gray2,
+    color: theme.color.gray5,
     fontWeight: '500',
     lineHeight: 21,
     letterSpacing: -0.5,
