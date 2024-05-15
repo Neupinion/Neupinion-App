c1eebc003045c0b47e5c9aca2951cd12aa105e56
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-05-15 14:57:19 +0900

    feat: api 함수 수정

--- src/features/opinion/remotes/individualVote.ts
+++ src/features/opinion/remotes/individualVote.ts
@@ -2,7 +2,11 @@ import { client } from '../../../shared/remotes/axios';
 import { OpinionParagraphId } from '../../../shared/types/news';
 
 export const getOpinionParagraph = async (issueId: number, viewMode: string) => {
-  return await client.get<OpinionParagraphId[]>(`/reprocessed-issue/${issueId}/opinion/paragraph`, {
-    params: { viewMode: viewMode },
-  });
+  const { data } = await client.get<OpinionParagraphId[]>(
+    `/reprocessed-issue/${issueId}/opinion/paragraph`,
+    {
+      params: { viewMode: viewMode },
+    },
+  );
+  return data;
 };
