6e346724476af8dbdebd1353672ba0630a5e2357
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-05-26 01:25:26 +0900

    feat: 소셜 로그인 버튼 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { ImageSourcePropType, StyleSheet, Text, View } from 'react-native';
+import { ImageSourcePropType, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import theme from '../shared/styles/theme';
 import { WINDOW_WIDTH } from '../shared/constants/display';
 import NeupTextIcon from '../assets/icon/neuplogin.svg';
+import GoogleIcon from '../assets/icon/googleicon.svg';
+import KaKaoIcon from '../assets/icon/kakaologo.svg';
+import AppleIcon from '../assets/icon/appleicon.svg';
 import { WithLocalSvg } from 'react-native-svg/css';
 import {
   APP_TEXT_ICON_HEIGHT,
   APP_TEXT_ICON_WIDTH,
 } from '../features/auth/constants/appTextIconSize';
 import fontFamily from '../shared/styles/fontFamily';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const icons = [KaKaoIcon, GoogleIcon, AppleIcon];
+
 const LoginPage = () => {
   return (
     <View style={styles.container}>
@@ -19,7 +26,22 @@ const LoginPage = () => {
           asset={NeupTextIcon as ImageSourcePropType}
         />
         <Text style={styles.normalText}>뉴피니언에 오신 것을 환영합니다.</Text>
-        <View style={styles.socialButtonContainer} />
+        <View style={styles.socialButtonContainer}>
+          {['카카오톡으로 로그인', '구글로 로그인', 'Apple로 로그인'].map((text, index) => (
+            <LinearGradient
+              key={index}
+              start={{ x: 2, y: 1 }}
+              end={{ x: -1, y: 1 }}
+              colors={theme.gradient.gradient1}
+              style={styles.gradientBorder}
+            >
+              <TouchableOpacity style={styles.socialButton}>
+                <WithLocalSvg width={21} height={21} asset={icons[index] as ImageSourcePropType} />
+                <Text style={styles.socialText}>{text}</Text>
+              </TouchableOpacity>
+            </LinearGradient>
+          ))}
+        </View>
       </View>
     </View>
   );
@@ -48,6 +70,31 @@ const styles = StyleSheet.create({
   socialButtonContainer: {
     display: 'flex',
     width: '100%',
+    marginTop: 60,
+    gap: 16,
+  },
+  socialButton: {
+    width: '100%',
+    borderRadius: 5,
+    height: 48,
+    backgroundColor: theme.color.gray2,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    padding: 13,
+  },
+  gradientBorder: {
+    padding: 1,
+    borderRadius: 7,
+  },
+  socialText: {
+    color: theme.color.white,
+    fontFamily: fontFamily.pretendard.medium,
+    fontSize: 15,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    lineHeight: 22.5,
+    letterSpacing: -0.45,
+    marginLeft: 12,
   },
 });
 
