ab635ec23482a08227e3f561087d936c21e8e2fc
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-05-28 00:31:20 +0900

    feat: app.json 및 여러 파일 수정

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { DateProvider } from './src/features/date/provider/DateProvider';
 import { useCachedResources } from './src/useCachedResources';
 import Navigation from './src/Navigation';
 import { RecoilRoot } from 'recoil';
-import OpinionMainPage from "./src/pages/OpinionMainPage";
 export default function App(): JSX.Element | null {
   const isLoaded = useCachedResources();
 
