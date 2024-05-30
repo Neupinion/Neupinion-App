af6d80b77347b0d0b9fdbcce11cee56359fa6452
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-05-30 23:02:23 +0900

    Feat/#99 투표 페이지 UI 및 관련 api 연동 마무리 (#105)
    * feat: top5의견 추가하기
    
    * feat: 의견보기 페이지 윗부분 api 연결
    
    * feat: 네비게이션 삭제
    
    * feat: 네비게이션 복구
    
    * feat: 네비게이션 수정해야 함
    
    * feat: 스크롤 뷰 수정

--- src/pages/OpinionMainPage.tsx
+++ src/pages/OpinionMainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -16,14 +16,31 @@ import { WithLocalSvg } from 'react-native-svg/css';
 import MainArrowLeftSvg from '../assets/icon/mainarrowLeft.svg';
 import { WINDOW_WIDTH } from '../shared/constants/display';
 import { mainCategories } from '../shared/constants/opinionCategory';
+import TopOpinionSlider from '../features/vote/components/TopOpinionSlider';
+import { formatDate } from '../features/remakeissue/constants/formatDate';
+import SeeOriginalSvg from '../assets/icon/seeOriginal.svg';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { RootStackParamList } from '../rootStackParamList';
+import useFetch from '../shared/hooks/useFetch';
+import { getReprocessedIssueContent } from '../features/remakeissue/remotes/reprocessedIssueContent';
 
 const OpinionMainPage = () => {
+  type ScreenRouteProp = RouteProp<RootStackParamList, 'OpinionMainPage'>;
+  const route = useRoute<ScreenRouteProp>();
+  const id: number = route.params.id;
+  const fetchReprocessedIssue = () => getReprocessedIssueContent(id);
+  const { data: reprocessedIssue, fetchData } = useFetch(fetchReprocessedIssue, false);
+
   const [activeMainCategory, setActiveMainCategory] = useState('전체');
   const SelectMainCategory = (category: string) => {
     setActiveMainCategory(category);
   };
+
+  useEffect(() => {
+    void fetchData();
+  }, []);
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
       <PageHeader
         leftIcons={
           <TouchableOpacity>
@@ -34,32 +51,55 @@ const OpinionMainPage = () => {
         RightIcons={null}
       />
       <View style={styles.headerUnderLine} />
-      <View style={styles.divideLine}></View>
-      <View style={styles.mainCategoryTop}>
-        {mainCategories.map((category) => (
-          <TouchableOpacity
-            key={category}
-            style={styles.mainCategory}
-            onPress={() => SelectMainCategory(category)}
-          >
-            <Text style={[styles.baseText, activeMainCategory === category && styles.activeText]}>
-              {category}
-            </Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-      <View>
-        <View style={styles.headerUnderLine} />
-        {activeMainCategory == mainCategories[0] && <View style={styles.selectedAll} />}
-        {activeMainCategory == mainCategories[1] && <View style={styles.selectedParagraph} />}
-      </View>
+      <ScrollView>
+        {reprocessedIssue && (
+          <View style={{ marginTop: 18 }}>
+            <Text style={styles.titleText}>{reprocessedIssue.title}</Text>
+            <View style={styles.titleUnderContainer}>
+              <View style={{ flexDirection: 'row' }}>
+                <View style={styles.tagBox}>
+                  <Text style={styles.tagText}>{reprocessedIssue.category}</Text>
+                </View>
+                <Text style={styles.dateText}>{formatDate(reprocessedIssue.createdAt)}</Text>
+              </View>
+              <TouchableOpacity style={styles.headerSvg} onPress={() => {}}>
+                <WithLocalSvg
+                  width={79}
+                  height={30}
+                  asset={SeeOriginalSvg as ImageSourcePropType}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+        <TopOpinionSlider id={1} />
+        <View style={styles.divideLine}></View>
+        <View style={styles.mainCategoryTop}>
+          {mainCategories.map((category) => (
+            <TouchableOpacity
+              key={category}
+              style={styles.mainCategory}
+              onPress={() => SelectMainCategory(category)}
+            >
+              <Text style={[styles.baseText, activeMainCategory === category && styles.activeText]}>
+                {category}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View>
+          <View style={styles.headerUnderLine} />
+          {activeMainCategory == mainCategories[0] && <View style={styles.selectedAll} />}
+          {activeMainCategory == mainCategories[1] && <View style={styles.selectedParagraph} />}
+        </View>
 
-      {activeMainCategory === mainCategories[0] ? (
-        <TotalOpinionCategory issueId={1} />
-      ) : (
-        <ParagraphOpinionCategory issueId={1} />
-      )}
-    </ScrollView>
+        {activeMainCategory === mainCategories[0] ? (
+          <TotalOpinionCategory issueId={1} />
+        ) : (
+          <ParagraphOpinionCategory issueId={1} />
+        )}
+      </ScrollView>
+    </View>
   );
 };
 
@@ -67,6 +107,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.color.black,
+  },
+  titleText: {
+    paddingHorizontal: 26,
+    fontFamily: fontFamily.pretendard.bold,
+    fontSize: 20,
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: 25.5,
+    letterSpacing: -0.6,
+    color: theme.color.gray7,
+    width: '100%',
   },
   mainCategoryTop: {
     flexDirection: 'row',
@@ -133,6 +184,45 @@ const styles = StyleSheet.create({
   activityIndicator: {
     flex: 1,
     alignSelf: 'center',
+  },
+  titleUnderContainer: {
+    width: WINDOW_WIDTH,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 26,
+    marginTop: 12,
+    marginBottom: 25,
+  },
+  tagBox: {
+    display: 'flex',
+    paddingVertical: 2,
+    paddingHorizontal: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    backgroundColor: 'rgba(126, 88, 233, 0.20)',
+    marginRight: 8,
+  },
+  tagText: {
+    color: theme.color.main,
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    lineHeight: 21,
+    letterSpacing: -0.42,
+  },
+  dateText: {
+    color: theme.color.gray5,
+    fontStyle: 'normal',
+    fontSize: 12,
+    fontWeight: '500',
+    lineHeight: 18,
+    letterSpacing: -0.5,
+    marginTop: 2,
+  },
+  headerSvg: {
+    justifyContent: 'flex-end',
   },
 });
 
