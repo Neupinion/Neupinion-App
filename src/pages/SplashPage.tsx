c3361e631204f1050867c4ae9210e236ea9dba9a
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-06-05 17:04:46 +0900

    fix: NativeEvent를 이용해서, 인덱스 조절하는 함수 추가

--- src/pages/SplashPage.tsx
+++ src/pages/SplashPage.tsx
@@ -10,6 +10,8 @@ import {
   Text,
   ListRenderItem,
   Image,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
 } from 'react-native';
 import theme from '../shared/styles/theme';
 import { WINDOW_WIDTH } from '../shared/constants/display';
@@ -30,6 +32,12 @@ const SplashPage: React.FC = () => {
     } else {
       navigation.navigate('LoginPage');
     }
+  };
+
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const offsetX = event.nativeEvent.contentOffset.x;
+    const index = Math.round(offsetX / WINDOW_WIDTH);
+    setCurrentIndex(index);
   };
 
   const renderItem: ListRenderItem<SplashDataItem> = ({ item }) => (
@@ -58,6 +66,8 @@ const SplashPage: React.FC = () => {
         ref={flatListRef}
         keyExtractor={(item) => item.key}
         showsHorizontalScrollIndicator={false}
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
       />
       <TouchableOpacity style={styles.button} onPress={handleNext}>
         <Text style={styles.buttonText}>다음</Text>
