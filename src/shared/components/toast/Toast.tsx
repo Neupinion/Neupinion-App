9669f8ee15131bf5f2884f8c424431bf065b3d7a
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-06-11 18:47:40 +0900

    Feat/#128 의견 쓰기 페이지에서, 필수 요소가 입력되지 않았을 때의 토스트를 제작한다 (#130)
    * feat: Toast 컴포넌트 제작
    
    * feat: createContext를 사용하여, 전역으로 관리되는 Toast 제작
    
    * feat: Toast스타일 및 Context를 이용해서, 의견 포스트 페이지에 적용
    
    * feat: Toast의 움직임 수정 및 개발 완료
    
    * Update src/shared/components/toast/ToastContext.tsx
    
    Co-authored-by: Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>

---
+++ src/shared/components/toast/Toast.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import { Animated, ImageSourcePropType, StyleSheet, Text } from 'react-native';
+import theme from '../../styles/theme';
+import { WINDOW_WIDTH } from '../../constants/display';
+import fontFamily from '../../styles/fontFamily';
+import { WithLocalSvg } from 'react-native-svg/css';
+import ToastIcon from '../../../assets/icon/warntoast.svg';
+import { Easing } from 'react-native';
+
+interface ToastProps {
+  message: string;
+  onHide: () => void;
+}
+
+const Toast = ({ message, onHide }: ToastProps) => {
+  const [show, setShow] = useState(false);
+  const [translateY] = useState(new Animated.Value(-100));
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    if (message && !isAnimating) {
+      setIsAnimating(true);
+      setShow(true);
+      Animated.timing(translateY, {
+        toValue: 70,
+        duration: 400,
+        easing: Easing.inOut(Easing.ease),
+        useNativeDriver: true,
+      }).start(() => {
+        setTimeout(() => {
+          Animated.timing(translateY, {
+            toValue: -100,
+            duration: 400,
+            easing: Easing.inOut(Easing.ease),
+            useNativeDriver: true,
+          }).start(() => {
+            setShow(false);
+            setIsAnimating(false);
+            onHide();
+          });
+        }, 2000);
+      });
+    }
+  }, [message]);
+
+  if (!show) return null;
+
+  return (
+    <Animated.View style={[styles.toast, { transform: [{ translateY }] }]}>
+      <WithLocalSvg height={22} asset={ToastIcon as ImageSourcePropType} />
+      <Text style={styles.message}>{message}</Text>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  toast: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: theme.color.gray3,
+    width: WINDOW_WIDTH - 52,
+    marginHorizontal: 26,
+    paddingHorizontal: 16,
+    height: 50,
+    gap: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    borderRadius: 40,
+    zIndex: 1000,
+  },
+  message: {
+    color: theme.color.white,
+    fontFamily: fontFamily.pretendard.bold,
+    fontSize: 14,
+    fontWeight: 700,
+    lineHeight: 21,
+    letterSpacing: -0.42,
+  },
+});
+
+export default Toast;
